523c42ff63ac9059116b778d00b09f9f019f651a
Matt Mencel <mr-mencel@wiu.edu>
2013-09-27 12:56:46 -0500

    stop tomcat6

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,7 +1,7 @@
 
 
 
-disabled_svcs = ['clamd', 'postfix', 'spamassassin']
+disabled_svcs = ['clamd', 'postfix', 'spamassassin', 'tomcat6']
 disabled_svcs.each do |svc|
 	service svc do
 		action [ :disable, :stop ]
